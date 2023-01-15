f0dbdc16764d9d0508293dd480c0f3c760f3ee0f
sidiki97 <62025524+sidiki97@users.noreply.github.com>
2023-01-15 17:23:27 +0000

    Add libs

--- SmallProblems.js
+++ SmallProblems.js
@@ -1,28 +1,4 @@
 //const prompt = require('prompt-sync')();
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-
-
-
-function area_meters(l, w){
-    
-    return l*w;
-}
-
-// let l = prompt("Enter the length of the room in meters: ");
-
-// let w = prompt("Enter the width of the room in meters: ");
-
-let l = 2.0;
-let w = 3.0;
-
-readline.question('Enter length and width?', (l,w) => {
-    console.log(`The area of the room is ${area_meters(l,w)} square meters.!`);
-
-  });
-
-console.log(`The area of the room is ${area_meters(l,w)} square meters.`);
-
-let o = 1;
+let rlSync = require('readline-sync');
+let name = rlSync.question("What's your name?\n");
+console.log(`Good Morning, ${name}!`);
